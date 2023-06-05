b657baeefbbef47dc0386afd44c3cf44b26c7d7d
Daniel Lewis <daniel.robert.lewis@gmail.com>
2023-06-05 09:36:07 +0000

    asdf

--- dfaas_container_python3/workdir/gowalla.py
+++ dfaas_container_python3/workdir/gowalla.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-import time
-import datetime
+import pymongo
+import os
+conn = pymongo.MongoClient(os.environ['MONGODB_HOST'], int(os.environ['MONGODB_PORT']), username=os.environ['MONGODB_USER'], password=os.environ['MONGODB_PASSWORD'])
+coll = conn.test.dfaas_functions
 
-while True:
-  time.sleep(1)
-  print(datetime.datetime.now())
+fn_rec = conn.test.dfaas_functions.find_one({ 'name': os.environ['FUNCTION_NAME'] })
+fn_code = fn_rec['code']
+exec(fn_code)
+
