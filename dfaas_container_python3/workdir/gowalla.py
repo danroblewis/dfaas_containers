c72d0bfc4a54e0ea044b2646b3498e10cd6eacb6
Daniel Lewis <daniel.robert.lewis@gmail.com>
2023-06-07 05:34:29 +0000

    what

--- dfaas_container_python3/workdir/gowalla.py
+++ dfaas_container_python3/workdir/gowalla.py
@@ -1,11 +1,54 @@
 #!/usr/bin/python3
 
+import kafka
 import pymongo
 import os
+import json
+import time
+
 conn = pymongo.MongoClient(os.environ['MONGODB_HOST'], int(os.environ['MONGODB_PORT']), username=os.environ['MONGODB_USER'], password=os.environ['MONGODB_PASSWORD'])
 coll = conn.test.dfaas_functions
-
-fn_rec = conn.test.dfaas_functions.find_one({ 'name': os.environ['FUNCTION_NAME'] })
+filter = { 'name': os.environ['FUNCTION_NAME'] }
+fn_rec = conn.test.dfaas_functions.find_one(filter)
+print('fn record', fn_rec)
 fn_code = fn_rec['code']
-exec(fn_code)
+
+consumer = None
+emit = None
+delay = None
+
+# if this function has an INPUT_TOPIC defined, read and process via input topic
+if 'INPUT_TOPIC' in os.environ:
+    consumer = kafka.KafkaConsumer(os.environ['INPUT_TOPIC'], bootstrap_servers=[ os.environ['KAFKA_ADDRESS'] ])
+
+if 'OUTPUT_TOPIC' in os.environ:
+    producer = kafka.KafkaProducer(bootstrap_servers=[ os.environ['KAFKA_ADDRESS'] ])
+    def _emit(outrec):
+        producer.send(os.environ['OUTPUT_TOPIC'], outrec)
+    emit = _emit
+
+elif 'TIMER' in os.environ:
+    delay = int(os.environ['TIMER'])
+
+
+
+if consumer:
+    i = 0
+    for rec in consumer:
+        print("message: ", i)
+        i += 1
+        exec(fn_code)
+elif delay:
+    i = 0
+    while True:
+        print("message: ", i)
+        i += 1
+        exec(fn_code)
+        time.sleep(delay)
+else:
+    print("dude wtf, there is literally nothing for me to do.")
+    print("like, you didn't give me an INPUT_TOPIC, and you didn't give me a TIMER (which I woudl use for a sleep value")
+    print("you gotta gie me something.")
+    print("this is just fucking ridiculous.")
+
 
